8e2a25d5b0e06ae5c6223793a4dd8f569db349a4
Luka <manialuka19@gmail.com>
2024-12-01 22:52:11 +0400

    update id system, add delete functionality

--- manage.py
+++ manage.py
@@ -12,8 +12,19 @@ add_parser = subparser.add_parser('add', help='Add a new expense')
 add_parser.add_argument('-d', '--description', type=str, help='Description of the expense', required=True)
 add_parser.add_argument('-a', '--amount', type=float, help='Amount of the expense', required=True)
 
+delete_parser = subparser.add_parser('delete', help='Delete an archived expense by providing its ID')
+delete_parser.add_argument('id', type=int, help='ID  of the expense to delete')
+
+list_parser = subparser.add_parser('list', help='List all the expenses')
+
+
 args = parser.parse_args()
 print(args)
 
-if args.command == 'add':
-    Expense.objects.create(args.amount, args.description)
+match args.command:
+    case 'add':
+        Expense.objects.create(args.amount, args.description)
+    case 'list':
+        Expense.objects.display()
+    case 'delete':
+        Expense.objects.remove(args.id)
--- models.py
+++ models.py
@@ -7,19 +7,47 @@ class ExpenseManager:
         try:
             open('db.csv', 'r')
 
+            try:
+                open('id_counter_file.txt', 'r')
+
+            except FileNotFoundError:
+                with open('id_counter_file.txt', 'w') as counter:
+                    counter.write(str(len(cls.all())))
+
         except FileNotFoundError:
             open('db.csv', 'w')
+
+            with open('id_counter_file.txt', 'w') as counter:
+                counter.write('0')
     
     # Saves a new Expense instance into the database
     @classmethod
     def create(cls, amount, description):
-        with open('db.csv', 'r') as database: 
-            id = len(database.readlines()) + 1
+        with open('id_counter_file.txt', 'r') as database: 
+            id_data = database.read().strip()
+            id = int(id_data.replace('\n', ''))
+        with open('id_counter_file.txt', 'w') as database: 
+            database.write(str(id + 1))
 
         created_at = date.today()
 
         with open('db.csv', 'a') as database:
             database.write(f'{id}, {amount}, {description}, {created_at}\n')
+
+    # Removes data of Expense linked to the provided ID
+    @classmethod
+    def remove(cls, id):
+        with open('db.csv', 'r') as database:
+            lines = database.readlines()
+            
+        with open('db.csv', 'w') as database:
+            removed = False
+            for line in lines:
+                columns = line.split(',')
+                if int(columns[0]) != id:
+                    database.write(line)
+                else:
+                    removed = True
 
     # Returns all the Expense instances from the database
     @classmethod
@@ -43,7 +71,7 @@ class ExpenseManager:
         print('********** EXPENSES **********')
         print(f'{len(expenses)} expense{'' if len(expenses) == 1 else 's'} in total', end='\n\n')
         for expense in expenses:
-            print(f'{expense.amount}$ -- {expense.description} -- id: {expense.id} -- date: {expense.created_at}')
+            print(f'{expense.amount}$ -- {expense.description} -- id: {expense.id} -- date: {expense.created_at}', end=f'{'\n\n' if expense != expenses[-1]  else '\n'}')
 
 class Expense:
     objects = ExpenseManager
@@ -53,5 +81,3 @@ class Expense:
         self.amount = amount
         self.description = description
         self.created_at = created_at
-
-ExpenseManager.display()
