d5d37a127e0db817e46cac57f107631133105d75
Luka <manialuka19@gmail.com>
2024-12-02 10:32:08 +0400

    add an option to add budgets for each month and some small updates

--- manage.py
+++ manage.py
@@ -1,8 +1,15 @@
 import argparse
+import calendar
 
 from models import  Expense, ExpenseManager
 
 ExpenseManager.database()
+
+month_names = calendar.month_name[1:]
+months = {}
+
+for i in range(12):
+    months[i + 1] = month_names[i]
 
 parser = argparse.ArgumentParser(description='Manage your expenses')
 
@@ -13,9 +20,19 @@ add_parser.add_argument('-d', '--description', type=str, help='Description of th
 add_parser.add_argument('-a', '--amount', type=float, help='Amount of the expense', required=True)
 
 delete_parser = subparser.add_parser('delete', help='Delete an archived expense by providing its ID')
-delete_parser.add_argument('id', type=int, help='ID  of the expense to delete')
+delete_parser.add_argument('--id', type=int, help='ID  of the expense to delete')
 
 list_parser = subparser.add_parser('list', help='List all the expenses')
+
+summary_parser = subparser.add_parser('summary', help='See the summary of your expenses')
+summary_parser.add_argument('-m', '--month', type=int, help='Select a month to see expenses specific for it', required=False)
+
+budget_parser = subparser.add_parser('budget', help='Manage your monthly budgets')
+
+budget_subparser = budget_parser.add_subparsers(dest='budget_command', help='Available  commands')
+set_parser = budget_subparser.add_parser('set', help='Set a new budget for certain month')
+set_parser.add_argument('-m', '--month', type=int, help='Month to set the budget for', required=True)
+set_parser.add_argument('-a', '--amount', type=int, help='Amount of the budget', required=True)
 
 
 args = parser.parse_args()
@@ -28,3 +45,11 @@ match args.command:
         Expense.objects.display()
     case 'delete':
         Expense.objects.remove(args.id)
+    case 'summary':
+        if args.month == None:
+            print(f'Summary of all expenses: {Expense.objects.summary()}$')
+        else:
+            print(f'Summary of expenses in {months[args.month]}: {Expense.objects.summary(month=args.month)}$')
+    case 'budget':
+        if args.budget_command == 'set':
+            Expense.set_budget(months[args.month], args.amount)
--- models.py
+++ models.py
@@ -1,7 +1,8 @@
-from datetime import date
+from datetime import datetime, date
+import calendar
 
 class ExpenseManager:
-    # Checks if databse (db.csv) file already exists. If it doesnt, the function creates it.
+    # Checks if databse (db.csv) file already exists. If it doesnt, the function creates it. Does the same for budgets.csv
     @classmethod
     def database(cls):
         try:
@@ -19,6 +20,15 @@ class ExpenseManager:
 
             with open('id_counter_file.txt', 'w') as counter:
                 counter.write('0')
+
+        try:
+            open('budgets.csv', 'r')
+        except FileNotFoundError:
+            with open('budgets.csv', 'w') as budgets:
+                months = calendar.month_name[1:]
+                for month in months:
+                    budgets.write(f'{month},N/A\n')
+
     
     # Saves a new Expense instance into the database
     @classmethod
@@ -32,7 +42,7 @@ class ExpenseManager:
         created_at = date.today()
 
         with open('db.csv', 'a') as database:
-            database.write(f'{id}, {amount}, {description}, {created_at}\n')
+            database.write(f'{id},{amount},{description},{created_at}\n')
 
     # Removes data of Expense linked to the provided ID
     @classmethod
@@ -65,19 +75,47 @@ class ExpenseManager:
         return expenses
 
     @classmethod
+    def summary(cls, month=0):
+        expenses = cls.all()
+        summary = 0
+
+        if month == 0:
+            for expense in expenses:
+                summary += expense.amount
+            return summary
+
+        for expense in expenses:
+            if expense.created_at.month == month:
+                summary += expense.amount
+
+        return summary
+
+    @classmethod
     def display(cls):
         expenses = cls.all()
         
         print('********** EXPENSES **********')
         print(f'{len(expenses)} expense{'' if len(expenses) == 1 else 's'} in total', end='\n\n')
         for expense in expenses:
-            print(f'{expense.amount}$ -- {expense.description} -- id: {expense.id} -- date: {expense.created_at}', end=f'{'\n\n' if expense != expenses[-1]  else '\n'}')
+            print(f'{expense.amount}$ -- {expense.description} -- id: {expense.id} -- date: {expense.created_at.date()}', end=f'{'\n\n' if expense != expenses[-1]  else '\n'}')
 
 class Expense:
     objects = ExpenseManager
 
     def __init__(self, id, amount, description, created_at):
         self.id = id
-        self.amount = amount
+        self.amount = float(amount)
         self.description = description
-        self.created_at = created_at
+        self.created_at = datetime.strptime(created_at, '%Y-%m-%d')
+
+    @classmethod
+    def set_budget(cls, month, budget):
+        with open('budgets.csv', 'r') as budgets:
+            lines = budgets.readlines()
+
+        with open('budgets.csv', 'w') as budgets:
+            for line in lines:
+                if month != line.split(',')[0]:
+                    budgets.write(line)
+                else:
+                    budgets.write(f'{month},{budget}\n')
